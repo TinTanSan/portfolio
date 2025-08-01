91a0be86eb74359e8c082449545252846e5e126e
TinTanSan <tirthvpatel2003@gmail.com>
2025-08-01 16:18:37 +1000

    added a page to host resume

---
+++ src/app/resume/page.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Resume() {
+  return (
+    <div className='flex w-screen h-screen border-2'>
+        <title>Resume</title>
+        <iframe className='flex w-full h-full decoration-0' src='/Tirth_resume_cs.pdf'/>
+    </div>
+  )
+}
