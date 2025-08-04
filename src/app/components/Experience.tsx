a15b5eb83a14e955690a822c51c8d5785e87fffc
TinTanSan <tirthvpatel2003@gmail.com>
2025-08-05 09:26:34 +1000

    new component for experience

---
+++ src/app/components/Experience.tsx
@@ -0,0 +1,46 @@
+import React from 'react'
+import { ExperienceDetails } from '../utils/experiences'
+
+
+type props = {
+    experience: ExperienceDetails
+}
+
+
+export default function Experience({experience}: props) {
+  return (
+    <div className="flex flex-col border-2 px-2 items-center">
+        <div className="flex flex-col md:flex-row text-lg md:text-2xl font-bold w-full items-center md:justify-between">
+            <div>
+                {experience.title}
+            </div>
+            <div className='flex text-sm md:text-xl'>
+                {experience.company}
+            </div>
+        </div>
+        <div className='flex w-3/4 md:w-[98%] h-0.5 bg-black' />
+        <div className="text-xs md:text-sm">{experience.duration} | {experience.location}</div>
+        <div className="flex flex-wrap grow shrink text-[0.8rem] md:text-[0.95rem] text-wrap">
+            {experience.overview}
+        </div>
+        <div className='flex flex-col w-full h-full'>
+            <div className='flex w-full text-[0.8rem] md:text-[1rem]'>Contributions</div>
+            {experience.contributions.map((contribution, i)=>{
+                return (
+                    <ul key={i} className='flex flex-col list-inside'>
+                        <h3 className='flex font-bold pl-2 text-[0.8rem] md:text-lg'>{contribution.title}</h3>
+                        {contribution.details.map((detail, j)=>
+                            <div className='flex gap-1 pl-5 text-[0.75rem] md:text-[0.95rem]'>
+                                -
+                                <li className='flex' key={j}>
+                                    {detail}
+                                </li>
+                            </div>
+                        )}
+                    </ul>
+                )
+            })}
+        </div>
+    </div>
+  )
+}
