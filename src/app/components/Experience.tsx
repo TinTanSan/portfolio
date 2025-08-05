606436061487c6998fb2cedba3076e89ad3a739b
TinTanSan <tirthvpatel2003@gmail.com>
2025-08-05 12:10:51 +1000

    minor changes

--- src/app/components/Experience.tsx
+++ src/app/components/Experience.tsx
@@ -9,7 +9,7 @@ type props = {
 
 export default function Experience({experience}: props) {
   return (
-    <div className="flex flex-col border-2 px-2 items-center">
+    <div className="flex flex-col border-2 border-base-300 rounded-lg px-2 items-center p-2">
         <div className="flex flex-col md:flex-row text-lg md:text-2xl font-bold w-full items-center md:justify-between">
             <div>
                 {experience.title}
@@ -18,21 +18,21 @@ export default function Experience({experience}: props) {
                 {experience.company}
             </div>
         </div>
-        <div className='flex w-3/4 md:w-[98%] h-0.5 bg-black' />
-        <div className="text-xs md:text-sm">{experience.duration} | {experience.location}</div>
+        <div className='flex w-3/4 md:w-[98%] h-0.5 bg-base-200 gap-5' />
+        <div className="text-xs md:text-sm flex w-full">{experience.duration} | {experience.location}</div>
         <div className="flex flex-wrap grow shrink text-[0.8rem] md:text-[0.95rem] text-wrap">
             {experience.overview}
         </div>
         <div className='flex flex-col w-full h-full'>
             <div className='flex w-full text-[0.8rem] md:text-[1rem]'>Contributions</div>
-            {experience.contributions.map((contribution, i)=>{
+            {experience.contributions.map((contribution, j)=>{
                 return (
-                    <ul key={i} className='flex flex-col list-inside'>
+                    <ul key={j} className='flex flex-col list-inside'>
                         <h3 className='flex font-bold pl-2 text-[0.8rem] md:text-lg'>{contribution.title}</h3>
-                        {contribution.details.map((detail, j)=>
-                            <div className='flex gap-1 pl-5 text-[0.75rem] md:text-[0.95rem]'>
+                        {contribution.details.map((detail, k)=>
+                            <div key={k} className='flex gap-1 pl-5 text-[0.75rem] md:text-[0.95rem]'>
                                 -
-                                <li className='flex' key={j}>
+                                <li className='flex'>
                                     {detail}
                                 </li>
                             </div>
