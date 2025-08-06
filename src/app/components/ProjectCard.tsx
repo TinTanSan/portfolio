e81c871b9df7c49b50a9400c73f797b18dcd5a05
TinTanSan <tirthvpatel2003@gmail.com>
2025-08-07 08:27:20 +1000

    reduced height of card element

--- src/app/components/ProjectCard.tsx
+++ src/app/components/ProjectCard.tsx
@@ -13,11 +13,11 @@ type props = {
 export default function ProjectCard({projectDetails, id}:props) {
     
   return (
-    <div id={id} className='grid row-span-1 h-[75vh] col-span-1 grid-flow-row-dense grid-rows-9 grid-cols-1 bg-base-100  border-2 border-base-300 text-base-content shadow-xl rounded-lg gap-5 p-2 zoom-in-div'>
+    <div id={id} className='grid row-span-1 h-[70vh] col-span-1 grid-flow-row-dense grid-rows-9 grid-cols-1 bg-base-100  border-2 border-base-300 text-base-content shadow-xl/30 shadow-zinc-500 rounded-lg gap-5 p-2 py-4 zoom-in-div'>
         <div className='grid row-span-1 w-full h-fit justify-center text-xl font-bold'>{projectDetails.projectName}</div>
         <div className='grid row-span-8 grid-rows-6 w-full h-full'>
             <div className='grid row-span-5 w-full h-full overflow-hidden overflow-ellipsis text-lg'>{projectDetails.projectOverview}</div>
-            <div className='grid grid-flow-col-dense grid-cols-1 grid-rows-2 w-full justify-between'>
+            <div className='grid grid-flow-col-dense grid-cols-1 grid-rows-2 gap-1 w-full justify-between'>
               <div className='grid col-span-full row-span-1 grid-rows-1 grid-cols-3 w-full items-center'>
                 <div className='grid col-span-1'>Tech Stack:</div>
                 <div className='grid grid-flow-col-dense items-center col-span-2 h-fit w-full overflow-x-auto'>
