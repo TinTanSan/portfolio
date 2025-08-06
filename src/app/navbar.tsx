53cc55a30dc7395ffc365d33f461efbf3f3ef65f
TinTanSan <tirthvpatel2003@gmail.com>
2025-08-07 08:25:21 +1000

    fixed compatibility issue with chrome mobile where the collapse feature did not collapse div and the div was still using up certain width

--- src/app/navbar.tsx
+++ src/app/navbar.tsx
@@ -9,32 +9,29 @@ export default function Navbar() {
     useEffect(()=>{
         setWidth(window.screen.availWidth);
     },[]);
-
-
     const buttonStyle = 'bg-accent hover:bg-accent-hover hover:scale-110 transition-all duration-500 flex h-8 items-center justify-center px-5 rounded-lg text-accent-content'
+
     return (
         <div className='flex w-[98%] z-10 sticky rounded-lg top-1 items-center p-2 h-12 bg-base-100 border-2 border-base-300 justify-center'>
-            <div className='flex collapse md:visible md:flex-row w-full gap-2'>
+            <div className='flex collapse w-0 md:visible md:flex-row md:w-full gap-2'>
                 <Link href={"#aboutMe"}  className={buttonStyle}>About Me</Link>
                 <Link href={"#projects"} className={buttonStyle}>Projects</Link>
                 <Link href={"#experience"} className={buttonStyle}>Experience</Link>
             </div>
-            <div className='flex visible md:collapse flex-row w-full gap-0'>
+            <div className='flex visible md:collapse flex-row w-fit gap-0'>
                 <button onClick={()=>{setOpenHamburger(!openHamburgerMenu)}}>
-                    <Image src={"menu.svg"} alt='Menu' className='z-50 border-2' width={40} height={40}/>
+                    <Image src={"/menu.svg"} alt='Menu'  width={40} height={40}/>
                 </button>
             </div>
+            <div className='flex w-full' />
             <div className='flex w-fit'>
                 <Link href={"#contact"} className='bg-primary hover:bg-primary-hover flex h-8 items-center justify-center px-5 rounded-lg text-accent-content'>Contact</Link>
             </div>
-
-
-            {
-                openHamburgerMenu && 
-                <div className='flex fixed flex-col z-10 w-[98%] bg-base-100 border-2 border-base-300 rounded-lg p-5 h-fit gap-5 top-1 md:invisible'>
-                    <div className='flex justify-end w-full'>
-                    <button onClick={()=>{setOpenHamburger(!openHamburgerMenu)}}>
-                        <Image src={"close_menu.svg"} alt='Menu' className='z-50 border-2' width={40} height={40}/>
+            {openHamburgerMenu && 
+                <div className='flex flex-col fixed z-10 w-[98%] bg-base-100 border-2 border-base-300 rounded-lg pl-2 pb-5 h-fit gap-5 top-1 md:invisible'>
+                    <div className='flex w-full'>
+                    <button className='flex w-10 h-10' onClick={()=>{setOpenHamburger(!openHamburgerMenu)}}>
+                        <Image src={"/close_menu.svg"} alt='Menu' className='z-[60] border-2' width={40} height={40}/>
                     </button>
                     </div>
                     <div className='flex flex-col gap-2 w-[80%]'>
