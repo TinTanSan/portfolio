5b7f8535bb754972d34f60165bec7dc4b336d481
TinTanSan <tirthvpatel2003@gmail.com>
2025-08-08 07:04:22 +1000

    updated 3rd project to be an actual project rather than the dummy project

--- src/app/utils/projects.ts
+++ src/app/utils/projects.ts
@@ -51,7 +51,7 @@ export const projects:Array<ProjectDetails> = [
       projectOverview: `A minimalist cross-platform desktop application built to serve my needs for a secure offline 
         password manager. Taking inspiration from KeePass I set out to create a password manager which would be simple to 
         use, with help along the way.`,
-      techUsed: ['electron','typescript','tailwind'],
+      techUsed: ['electron','nextjs', 'typescript','tailwind'],
       projectInspriation:`
         For this project I wanted to make a secure password manager much like Keepass, which is accessible on all platforms.
         Having recently completed a course in full stack development I thought I'd use a framework that closely works with
@@ -64,24 +64,50 @@ export const projects:Array<ProjectDetails> = [
         the renderer process (the frontend GUI that the user can see). This project was the first time I had heard of the 
         concept.
         `,
-        ` Another challenge was searching key terms of a entry such as a username or an entry 
+        ` Another challenge was searching key terms of a entry such as a username or an entry title. If the entry is fully encrypted
+        using per-entry data encryption keys (DEKs) as is recommended then there is no way to actually search for either fields, as the mode of encryption
+        that i am using (AES-GCM)  uses randomised initalisation vectors which will ensure that no two plaintext and key pairs will result in the same 
+        ciphertext. To solve this, I utilised a method which is used by password managers like KeePass and Bitwarden which is to encrypt only the 
+        sensitive fields and to encrypt the rest of the data at a vault level.
+        `,
         `
-      ]
+          Performance is a key metric for all software created, no user wants to wait too long or have an app that is unresponsive. Unfortunately, 
+          encryption what could become quite a large file will take time. Given that I am using the browser side to encrypt and decrypt this leaves me
+          to have a disadvantage where i cannot utilse the multi-threading ability of AES-GCM. Given the constraints, I've opted to encrypt and decrypt
+          on request only, for example a vault level decrypt on login, and then entry level decryption only when the user specifically clicks on an entry;
+          and for encryption an entry level encryption on creation of new entries and a vault level encryption and a filestystem write at lock and close 
+          operations on the vault.
+        `
+      ],
+      currentProgress:`This project is still in the works. So far I have made the internal cryptographic functions that will handle encryption, decryption,
+      data key genration, and password based key derivation. Work has also commenced for handling reading vaults from the UI including a list of recently 
+      used vaults.
+      
+      `
     },
     
     
     {
-      projectName: "A project",
-      projectURL: "a_project",
-      projectOverview: `A minimalist cross-platform desktop application built to serve my needs for a secure offline 
-        password manager. Taking inspiration from KeePass I set out to create a password manager which would be simple to 
-        use, with help along the way. 
-        
-        This sentence should make this overview really really long. Try this sentence if the first one did not make it long enough; shoudl this also not work, try again.
-        This sentence should make this overview really really long. Try this sentence if the first one did not make it long enough; shoudl this also not work, try again.
-        This sentence should make this overview really really long. Try this sentence if the first one did not make it long enough; shoudl this also not work, try again.
-        This sentence should make this overview really really long. Try this sentence if the first one did not make it long enough; shoudl this also not work, try again.
-        This sentence should make this overview really really long. Try this sentence if the first one did not make it long enough; shoudl this also not work, try again.`,
-      techUsed: ['electron','typescript','tailwind'],
+      projectName: "The Money game",
+      projectURL: "the_money_game",
+      projectOverview: `A simple game about earning the most possible money from financial investments such as real estate
+      equities and intrest paying deposits.`,
+      projectInspriation:`
+        Long ago I played a game called Rat Race which had a similar premise of getting rich. The key distinction that I wanted to 
+        show in my game was the use of more realistic values for financial tools like deposit interest rates and real estate which in Rat Race
+        were picked in a way in which the player would earn money from very little money which is not always the case. More over, i wanted
+        to implement new mechanics such as taxation at the end of every year (12 month period) and an enhanced securities exchange with many more
+        companies to invest in.
+      `,
+      projectChallenges:[
+        `the main challenge was that I didn't want to build a backend for this as that would incur more cost to me, so instead I wanted to make use of 
+        localstorage and indexDB for storing all of the data on the client side`,
+        `storing the data on the client side has an implication that the users are able to tamper with the data if it is not securely stored. That being said,
+        it may also not make sense to encrypt data at every tick of the game as this would severly hinder the experience of the user.`,
+        `a challenge also arises in the fact that I am not a finance major by any means, and therefore I don't know everything there is to know in finance so 
+        some of the things that I put on there are what i found in my research when looking for certain real life financial mechanics`
+      ],
+      currentProgress:``,
+      techUsed: ['nextjs','typescript','tailwind'],
     },
 ]
