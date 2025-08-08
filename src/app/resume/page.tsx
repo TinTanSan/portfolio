724de7ea8d069866d11670a81acfda05eb67f4b5
TinTanSan <tirthvpatel2003@gmail.com>
2025-08-08 14:14:08 +1000

    minor change

--- src/app/resume/page.tsx
+++ src/app/resume/page.tsx
@@ -1,10 +1,9 @@
 import React from 'react'
-import { repoName } from '../../../next.config'
 export default function Resume() {
   return (
     <div className='flex w-screen h-screen border-2'>
         <title>Resume</title>
-        <iframe className='flex w-full h-full decoration-0' src={`${repoName}/Tirth_resume_cs.pdf`}/>
+        <iframe className='flex w-full h-full decoration-0' src={`/portfolio/Tirth_resume_cs.pdf`}/>
     </div>
   )
 }
