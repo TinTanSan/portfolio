8e4c161870d2d6ac67f9634abe6ccdd479496148
TinTanSan <tirthvpatel2003@gmail.com>
2025-08-08 13:52:01 +1000

    updated config to have repo name in the base path and asset prefixes

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,12 @@
 import type { NextConfig } from "next";
 
+export const repoName = 'portfolio';
 const nextConfig: NextConfig = {
   /* config options here */
-  distDir: './out'
+  distDir: './out',
+  output: 'export',
+  basePath: `/${repoName}`,
+  assetPrefix: `/${repoName}/`,
 };
 
 export default nextConfig;
--- src/app/resume/page.tsx
+++ src/app/resume/page.tsx
@@ -1,10 +1,9 @@
 import React from 'react'
-
 export default function Resume() {
   return (
     <div className='flex w-screen h-screen border-2'>
         <title>Resume</title>
-        <iframe className='flex w-full h-full decoration-0' src='/Tirth_resume_cs.pdf'/>
+        <iframe className='flex w-full h-full decoration-0' src={`/Tirth_resume_cs.pdf`}/>
     </div>
   )
 }
